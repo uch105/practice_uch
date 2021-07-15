3505c123c8314100185896c843ca8b72494ca4a8
uch105 <73928875+uch105@users.noreply.github.com>
2021-07-15 18:02:43 +0600

    Create chat_server.py

---
+++ chat_server.py
@@ -0,0 +1,55 @@
+import socket
+import threading
+
+#server connection data
+host=socket.gethostbyname(socket.gethostname())
+port=1050
+
+#starting server
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind((host,port))
+s.listen()
+
+#list of clients and nicknames
+clients=[]
+nicknames=[]
+
+#sending message to all connected clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+#handling clients
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            message = '{} has left the chat'.format(nickname).encode('ascii')
+            broadcast(message)
+            nicknames.remove(nickname)
+            break
+
+#receiving and listentin
+def receive():
+    while True:
+        client,address = s.accept()
+        print('Connection from {}'.format(str(address)))
+
+        client.send('Nick'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        broadcast('{} has joined!'.format(nickname).encode('ascii'))
+        client.send('Connected to server at {}'.format(host).encode('ascii'))
+
+        thread = threading.Thread(target=handle,args=(client,))
+        thread.start()
+
+receive()
